3c1aca82b4d11d4094b0228b8bb59666ae168680
phungthuan99 <58097690+phungthuan99@users.noreply.github.com>
2024-01-30 17:22:05 +0700

    added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     $('.add').on('click', function(e){
-        var items = $('input[type=radio]');
-        for(var i = 0;i<items.length;i++){
-            var check = items[i].checked;
-            if(check){
-                console.log(items[i].value);
+        if (confirm("Press a button!") == true) { 
+            var items = $('input[type=radio]');
+            for(var i = 0;i<items.length;i++){
+                var check = items[i].checked;
+                if(check){
+                    console.log(items[i].value);
+                    addKQ(items[i].value)
+                }
             }
         }
     });
@@ -12,14 +15,18 @@ $(document).ready(function(){
         var html = '';
         switch (name) {
             case 'chan':
-                
+                html = `<div class="item-chan"></div>`;
                 break;
             case 'le':
+                html = `<div class="item-le"></div>`;
                 break;
             case 'xap':
+                html = `<div class="item-xap"></div>`;
                 break;
-            default:
+            case 'tu':
+                html = `<div class="item-tu"></div>`;
                 break;
         }
+        $('.add-here').append(html)
     }
 })
