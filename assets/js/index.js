3d09015217dee3e1e9448333dc57a027cd04b9e3
phungthuan99 <58097690+phungthuan99@users.noreply.github.com>
2025-11-03 12:35:55 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("#openModalBtn").onclick(function(){
+    $('#openModalBtn').on('click',function(){
         $("#myModal").show();
     })
-    $(".close").onclick(function(){
+    $('.close').on('click',function(){
         $("#myModal").hide();
     })
 })
