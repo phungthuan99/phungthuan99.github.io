f432463d9b439bbdb464bee8ef643b47da4a515b
phungthuan99 <58097690+phungthuan99@users.noreply.github.com>
2025-11-03 12:16:08 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,8 @@
 $(document).ready(function(){
-    $('.add').on('click', function(e){
-        if (confirm("Press a button!") == true) { 
-            var items = $('input[type=radio]');
-            for(var i = 0;i<items.length;i++){
-                var check = items[i].checked;
-                if(check){
-                    console.log(items[i].value);
-                    addKQ(items[i].value)
-                }
-            }
-        }
-    });
-    function addKQ(name){
-        var html = '';
-        switch (name) {
-            case 'chan':
-                html = `<div class="item-chan"></div>`;
-                break;
-            case 'le':
-                html = `<div class="item-le"></div>`;
-                break;
-            case 'xap':
-                html = `<div class="item-xap"></div>`;
-                break;
-            case 'tu':
-                html = `<div class="item-tu"></div>`;
-                break;
-        }
-        $('.add-here').append(html)
-    }
+    $("#openModalBtn").onclick(function(){
+        $("#myModal").show();
+    })
+    $(".close").onclick(function(){
+        $("#myModal").hide();
+    })
 })
